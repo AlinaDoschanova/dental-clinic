4ca1fc54fa3d44a91366b71750d23329479d9253
Alina Doschanova <doschanova.alina@gmail.com>
2018-01-03 21:14:13 +0600

    Главная: бургер заменён на крестик при открытом меню, изменены падинги для меню

--- src/js/script.js
+++ src/js/script.js
@@ -51,18 +51,6 @@ $(document).ready(function(){
   // Работа главного меню
   $('#menu-toggler').on('click', function(){
     $('#menu').toggleClass('main-nav__inner--shown');
+    $(this).toggleClass('burger--close');
   });
 });
-
-// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
